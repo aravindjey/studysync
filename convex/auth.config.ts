11e967db277334bebf8fb33f2565aef846b20838
aravindjey <aravindjayaseelan07@gmail.com>
2024-04-29 17:20:29 +0530

    2-adding webhooks

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+const authconfig = {
+  
+    providers: [
+      {
+        domain: "https://quality-aardvark-15.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+ };
+  
+export default authconfig;
